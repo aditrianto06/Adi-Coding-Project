979aa1774602df6d78da20fb2e096e992a9d5901
ADI TRIANTO SAPUTRA <122021845+aditrianto06@users.noreply.github.com>
2023-01-05 19:23:08 +0700

    Add files via upload

---
+++ Tubes STD/AdiAdi.h
@@ -0,0 +1,31 @@
+#ifndef ADIADI_H_INCLUDED
+#define ADIADI_H_INCLUDED
+
+#include <iostream>
+
+using namespace std;
+
+struct Mentor{
+    string Name;
+    Mentor *Next;
+};
+
+struct Apprentice{
+    string Name;
+    string Email;
+    int PerformenceScore;
+    Apprentice *Next;
+    Mentor *Parent;
+};
+
+void AddMentor(string Name);
+void ShowMentor();
+void AddApprentice(string Name, string Email, int PerformenceScore);
+void ShowApprentice();
+void Connect(string Apprentice, string Mentor);
+void ShowConnection();
+bool FindMentor(string Name);
+bool FindApprentice(string Name);
+void HighestPerformenceScore();
+
+#endif // ADIADI_H_INCLUDED
